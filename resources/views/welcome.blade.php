7add3b7ec8048a47d83febabaec7840dd13b32d7
Umer Inayat <umerinayat.369@gmail.com>
2021-08-29 02:07:30 +0500

    title updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Moving Streams - Tools | Convert Web Page Content to Full Image</title>
+    <title>Moving Streams - Tool Kit | Convert Web Page Content to Full Image</title>
 
     <style>
         html,
