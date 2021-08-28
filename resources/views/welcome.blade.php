52e2af544bac84b955aa47143a257b2516f29939
Umer Inayat <umerinayat.369@gmail.com>
2021-08-29 02:29:24 +0500

    added googal analytics scirpt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,19 @@
 
     <title>Moving Streams - Tool Kit | Convert Web Page Content to Full Image</title>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-56G7KJLN1D"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-56G7KJLN1D');
+    </script>
+
     <style>
         html,
         body {
@@ -37,7 +50,7 @@
         .form-signin {
             width: 100%;
             padding: 15px;
-           
+
         }
 
         .form-signin .checkbox {
@@ -85,23 +98,25 @@
             <div class="row">
                 <div class="col-sm-4">
                     <form id="form" class="form-signin text-center mt-5">
-                        <h1 class="h3 mb-3 fw-normal">Convert Web Page Content to Full Image, Enter Your Webpage URL.</h1>
+                        <h1 class="h3 mb-3 fw-normal">Convert Web Page Content to Full Image, Enter Your Webpage URL.
+                        </h1>
 
                         <div class="form-floating">
                             <input type="text" class="form-control" id="urlInput" name="url" placeholder="example.com">
                             <label for="urlInput">Webpage URL</label>
                         </div>
-                        <p id="errorText" class="mt-2 text-danger" style="display: none;"> <strong>Please Enter Valid Webpage URL</strong> </p>
+                        <p id="errorText" class="mt-2 text-danger" style="display: none;"> <strong>Please Enter Valid
+                                Webpage URL</strong> </p>
 
 
                         <button id="convertBtn" class="w-100 btn btn-lg btn-primary mt-3" type="submit">Convert
                             <div class="spinner-border spinner-border-sm text-light" style="display: none;" id="spinner" role="status">
                                 <span class="visually-hidden">Loading...</span>
                             </div>
-                        </button>   
+                        </button>
 
                         <a id="downlodBtn" class="w-100 btn btn btn-light mt-3" href="download" style="display: none;">
-                           Download
+                            Download
                         </a>
 
                     </form>
@@ -117,11 +132,12 @@
                 <p class="mt-5 mb-3 text-muted">&copy; {{date('Y')}} All Rights Reserved <a href="http://movingstreams.com">movingstreams.com</a> </p>
             </div>
         </div>
-        
+
     </main>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js" integrity="sha512-bZS47S7sPOxkjU/4Bt0zrhEtWx0y0CRkhEp8IckzK+ltifIIE9EMIMTuT/mEzoIMewUINruDBIR/jJnbguonqQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 
