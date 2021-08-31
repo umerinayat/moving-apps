0b4b730e27439b5844a2b774759ee4239b8c9a82
Umer Inayat <umerinayat.369@gmail.com>
2021-09-01 00:02:11 +0500

    typo fixed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -84,7 +84,7 @@
             <div class="p-5 mb-4 bg-light rounded-3">
                 <div class="container-fluid py-5">
                     <h1 class="display-4 fw-bold">Image Processing, AI, Machine Learning, Convert Files and Web Scraping Tool Kit & more...</h1>
-                    <p class="col-md-8 fs-5">We are working on creating on it.</p>
+                    <p class="col-md-8 fs-5">We are working on it.</p>
                     <small> More tools are coming soon. </small>
                 </div>
             </div>
